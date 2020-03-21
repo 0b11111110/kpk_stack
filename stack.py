11d3ef0d8f7403aebf5d7abee99324c1e50b2b66
0b11111110 <l.pheodor@gmail.com>
2020-03-21 15:18:04 +0300

    add prototipe stack.py

---
+++ stack.py
@@ -0,0 +1,14 @@
+class stack:
+    def __init__():
+        return []
+    def push():
+        pass
+    def pop():
+        pass
+    def top():
+        pass
+    def empty():
+        pass
+
+if __name__ == '__main__':
+    pass
